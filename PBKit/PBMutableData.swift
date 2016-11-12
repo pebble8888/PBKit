b7f7834c72162604ba0669d5f7cd77b3017bf187
pebble8888 <pebble8888@gmail.com>
2016-11-12 09:23:19 +0900

    PBMutableData

---
+++ PBKit/PBMutableData.swift
@@ -0,0 +1,15 @@
+//
+//  PBMutableData.swift
+//
+//  Created by pebble8888 on 2016/11/12.
+//  Copyright © 2016年 pebble8888. All rights reserved.
+//
+
+import Foundation
+
+extension NSMutableData {
+    func appendString(_ string: String) {
+        let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+        append(data!)
+    }
+}
