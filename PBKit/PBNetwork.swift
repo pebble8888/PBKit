a28191a4842f92464c34798121ea1d19a5ff0abc
pebble8888 <pebble8888@gmail.com>
2016-11-20 22:24:21 +0900

    PBNetwork,PBUIImage

---
+++ PBKit/PBNetwork.swift
@@ -0,0 +1,17 @@
+//
+//  PBNetwork.swift
+//
+//  Created by pebble8888 on 2016/11/20.
+//  Copyright © 2016年 pebble8888. All rights reserved.
+//
+
+import Foundation
+
+struct PBNetwork {
+    enum NextAction {
+        case succession
+        //case skip
+        case completion
+        case fail
+    }
+}
